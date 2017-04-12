f3cb7a757b2bf960a9cfe1a121d716535b687722
Jonathan Rauprich <rauprich.jonathan@googlemail.com>
2017-04-12 18:37:12 +0200

    fixing

--- app/lib/filter.js
+++ app/lib/filter.js
@@ -8,7 +8,7 @@ export default function (filterRules) {
     for (const rule of filterRules) {
       const match = desc.match(new RegExp(rule.from));
       if (match) {
-        return desc.replace(match[0], ` RE-AB-${match[1]}-${match[2]}`);
+        return desc.replace(match[0], ` RE-AB-${match[1]}-${match[2]} `);
       }
     }
     return desc;
